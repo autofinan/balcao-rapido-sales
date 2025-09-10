7f711e799f7e81576c8fd72a18fb5b6703ae5af0
autofinan <cortesttk711@gmail.com>
2025-09-10 19:48:01 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
+import { CartProvider } from "./context/CartContext";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </AuthProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>
 );
+
